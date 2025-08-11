23d9454d721168d8107599601f250924ec23faf0
isylsam <isylvestersam@gmail.com>
2025-08-11 22:07:13 +0100

    Ignore node_modules folder and remove it from repo

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["html/index.html"],
+  theme: {
+    extend: {
+      colors: {
+        "teal": "#e3f4fc",
+        "custom-green": "#dbda2f",
+        "dark-bg": "#133040"
+      },
+      fontFamily: {
+        body: ["Plus Jakarta Sans"],
+      },
+      borderRadius: {
+        '4xl': '2rem',
+        '5xl': '2.5rem',
+        '6xl': '3rem', 
+        '7xl': '4rem',
+        '8xl': '7.5rem',
+        '9xl': '10rem',
+      }
+    },
+  },
+  plugins: [],
+}
+
